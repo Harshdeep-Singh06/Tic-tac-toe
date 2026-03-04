6553ffe64c7fc2af49d7b0a8807c1a4176fc37be
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-03-04 20:19:35 +0530

    Add winning Patterns

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ for (let i = 0; i < box.length; i++) {
         }
     });
 }
+
+const winPatterns = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
+function checkWinner(){
+    for(let pattern of winPatterns){
+        let pos1 = box[pattern[0]].innerText
+        let pos2 = box[pattern[1]].innerText
+        let pos3 = box[pattern[2]].innerText
+        
+        if(pos1 !== "" && pos2 !== "" && pos3 !== "")
+    }
+}
