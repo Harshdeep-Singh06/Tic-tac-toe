6a79ec63e67416e71e65933f6279e23707390efa
Harshdeep Singh <harshdeepgirn2020@gmail.com>
2026-03-04 20:08:59 +0530

    Add logic to switch the player

--- script.js
+++ script.js
@@ -3,7 +3,14 @@ const box = document.querySelectorAll(".box");
 
 let currentPlayer = "X"
 for (let i = 0; i < box.length; i++) {
-    box[i].addEventListener('click', function(){
-        console.log("box clicked");
+    box[i].addEventListener('click', function(e){
+        if(this.innerText === ""){
+          this.innerText = currentPlayer
+          if(currentPlayer === "X"){
+            currentPlayer = "O"
+          }else{
+            currentPlayer = "X"
+          }
+        }
     });
 }
